ae5cc38acd7126102b62832dba4960bb8c40a555
Jatusart <toddsenesamrane@gmail.com>
2025-09-03 02:35:52 +0000

    first test on working code

---
+++ w4/app.js
@@ -0,0 +1,24 @@
+console.log('Hello from app.js! Your JavaScript is connected and running!');
+
+document.addEventListener("DOMContentLoaded", ()=> {
+// input of function
+//user information input aka their name
+// output of function
+const form = document.getElementById("Welcome Form");
+const nameInput = document.getElementById("nameInput");
+const greetingOutput = DocumentFragment.getElementById("Output Form");
+
+form.addEventListener("submit", (event) => {
+event.preventDefault();
+
+const name = nameInput.value.trim();
+
+if(name) {
+    greetingOutput.textContent = '${name}';
+    form.reset();
+} else {
+    greetingOutput.textContent = "Please input name."
+}
+ });
+});
+
