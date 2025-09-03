79e93b8ad1e777fccfb5a9c04a2a4b97db119f63
Jatusart <toddsenesamrane@gmail.com>
2025-09-03 03:32:37 +0000

    fix: updated code after peer collaboration

--- w4/app.js
+++ w4/app.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
 // input of function
 //user information input aka their name
 // output of function
-const form = document.getElementById("Welcome Form");
+const form = document.getElementById("WelcomeForm");
 const nameInput = document.getElementById("nameInput");
-const greetingOutput = DocumentFragment.getElementById("Output Form");
-
+const greetingOutput = document.getElementById("OutputForm");
+// Used AI to recheck code after first commit to make sure code was running in tandem with html
 form.addEventListener("submit", (event) => {
 event.preventDefault();
 
 const name = nameInput.value.trim();
 
 if(name) {
-    greetingOutput.textContent = `${name}`;
+    greetingOutput.textContent = `Hello, ${name}!`;
     form.reset();
 } else {
     greetingOutput.textContent = "Please input name."
